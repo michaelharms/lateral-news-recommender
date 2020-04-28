25ab5dd00976f6729261dd3c0e3a5851407c51a9
Michael Harms <michaelharms95@icloud.com>
2020-04-28 21:50:07 +0200

    refactor: Adjusting ArticleComponent props data in story and test

--- src/components/ArticleCard/ArticleCard.stories.js
+++ src/components/ArticleCard/ArticleCard.stories.js
@@ -8,13 +8,16 @@ export default {
 
 export const basic = () => {
   const article = {
-    similarity: 0.6865737438,
-    title: "Most Leaders Know Their Strengths",
-    url: "https://www.google.com",
-    published: "2020-03-19T09:15:10.000Z",
-    image: "https://placehold.it/200x200",
-    thumbnail: "https://placehold.it/20x20",
-    sourceName: "Harvard Business",
+    similarity: 0.6782692075,
+    title:
+      "Play To Your Strengths: 13 Even Better Ways To Improve Your Leadership Skills",
+    url:
+      "https://www.forbes.com/sites/forbescoachescouncil/2020/01/10/play-to-your-strengths-13-even-better-ways-to-improve-your-leadership-skills/",
+    published: "2020-01-10T18:15:00.000Z",
+    image:
+      "https://thumbor.forbes.com/thumbor/600x315/https%3A%2F%2Fblogs-images.forbes.com%2Fforbescoachescouncil%2Ffiles%2F2020%2F01%2F13up-blog-post-Play-To-Strengths-1200x1213.jpg",
+    thumbnail: "https://images.getnewsbot.com/17517611.jpg",
+    sourceName: "Forbes",
   };
 
   return <ArticleCard article={article} />;
--- src/components/ArticleCard/ArticleCard.test.js
+++ src/components/ArticleCard/ArticleCard.test.js
@@ -4,25 +4,30 @@ import ArticleCard from "./";
 
 test("renders article data", () => {
   const article = {
-    similarity: 0.6865737438,
-    title: "Most Leaders Know Their Strengths",
-    url: "https://www.google.com",
-    published: "2020-03-19T09:15:10.000Z",
-    image: "https://placehold.it/200x200",
-    thumbnail: "https://placehold.it/20x20",
-    sourceName: "Harvard Business",
+    similarity: 0.6782692075,
+    title:
+      "Play To Your Strengths: 13 Even Better Ways To Improve Your Leadership Skills",
+    url:
+      "https://www.forbes.com/sites/forbescoachescouncil/2020/01/10/play-to-your-strengths-13-even-better-ways-to-improve-your-leadership-skills/",
+    published: "2020-01-10T18:15:00.000Z",
+    image:
+      "https://thumbor.forbes.com/thumbor/600x315/https%3A%2F%2Fblogs-images.forbes.com%2Fforbescoachescouncil%2Ffiles%2F2020%2F01%2F13up-blog-post-Play-To-Strengths-1200x1213.jpg",
+    thumbnail: "https://images.getnewsbot.com/17517611.jpg",
+    sourceName: "Forbes",
   };
 
   const { getByText } = render(<ArticleCard article={article} />);
-  const similarity = getByText(/69%/i);
+  const similarity = getByText(/68%/i);
   expect(similarity).toBeInTheDocument();
 
-  const title = getByText(/Most Leaders Know Their Strengths/i);
+  const title = getByText(
+    /Play To Your Strengths: 13 Even Better Ways To Improve Your Leadership Skills/i
+  );
   expect(title).toBeInTheDocument();
 
-  const date = getByText(/2020-03-19/i);
+  const date = getByText(/2020-01-10/i);
   expect(date).toBeInTheDocument();
 
-  const source = getByText(/Harvard Business/i);
+  const source = getByText(/Forbes/i);
   expect(source).toBeInTheDocument();
 });
