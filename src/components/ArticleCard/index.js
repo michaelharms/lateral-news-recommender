c57026bbae5ca6d4615b71a88bf4d3b88a633949
Michael Harms <michaelharms95@icloud.com>
2020-04-29 11:43:38 +0200

    refactor: extracting ArticleCard styles into own file for more clarity and de-cluttering

--- src/components/ArticleCard/index.js
+++ src/components/ArticleCard/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Card from "../Card";
 import Link from "../Link";
-import styled from "styled-components";
-import { space } from "styled-system";
-import colors from "../../styles/colors";
 import formatDate from "../../utils/formatDate";
+import {
+  MinHeightCard,
+  ImageContainer,
+  TextContainer,
+  FlexRow,
+  Score,
+  Meta,
+  Image,
+  Thumbnail,
+} from "./styled";
 
 /**
  * Similar Article Card.
@@ -31,7 +37,7 @@ function ArticleCard({ article }) {
         <ImageContainer>
           <Image src={image} alt={`${title}`} />
         </ImageContainer>
-        <TextContainer p={3} pt={0}>
+        <TextContainer p={3}>
           <h3>{title}</h3>
 
           <FlexRow>
@@ -47,60 +53,6 @@ function ArticleCard({ article }) {
     </Link>
   );
 }
-
-// https://stackoverflow.com/a/19193308/11939354
-const ImageContainer = styled.div`
-  min-width: 98px;
-  min-height: 100%;
-  position: relative;
-  display: inline-block;
-  overflow: hidden;
-  margin: 0;
-`;
-
-const Image = styled.img`
-  display: block;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  min-height: 100%;
-  min-width: 100%;
-  transform: translate(-50%, -50%);
-`;
-
-const Thumbnail = styled.img`
-  width: 25px;
-  height: 25px;
-  object-fit: cover;
-  border-radius: 8px;
-`;
-
-const Score = styled.span`
-  font-weight: bold;
-  font-family: "Lato", sans-serif;
-  color: ${colors.green};
-`;
-
-const Meta = styled.span`
-  color: ${colors.gray};
-`;
-
-const MinHeightCard = styled(Card)`
-  min-height: 140px;
-  display: flex;
-`;
-
-const FlexRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const TextContainer = styled.div`
-  ${space};
-  color: ${colors.black};
-`;
 
 ArticleCard.propTypes = {
   /**
