288eee7e36e9bc365c0dc1d5b14c59cf75ad7a9a
Michael Harms <michaelharms95@icloud.com>
2020-04-29 17:00:10 +0200

    refactor: Wrapping built-in styled component types
    Without these wrapper, the storybook docs can not parse the prop types.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,20 @@
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-/**
- * Universal card component.
- */
-const Card = styled.div`
+const StyledDiv = styled.div`
   box-shadow: 0px 9px 17px -5px rgba(0, 0, 0, 0.25);
   border-radius: 14px;
   overflow: hidden;
   background-color: white;
 `;
+
+/**
+ * Universal card component.
+ */
+function Card({ children }) {
+  return <StyledDiv>{children}</StyledDiv>;
+}
 
 Card.propTypes = {
   /**
